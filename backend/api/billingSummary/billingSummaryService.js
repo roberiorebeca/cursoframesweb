1903ffdf0040acdb7d5e3ccddc4b830dea609079
Roberio Rebeca <roberio.rebeca@gmail.com>
2018-08-17 14:42:58 +0300

    Adicionado Arrys BillingCycle.aggregate

--- backend/api/billingSummary/billingSummaryService.js
+++ backend/api/billingSummary/billingSummaryService.js
@@ -3,13 +3,13 @@ const BillingCycle = require('../billingCycle/billingCycle')
 
 // Mais uma função middleware
 function getSummary(req, res) {
-  BillingCycle.aggregate({
+  BillingCycle.aggregate([{
     $project: {credit: {$sum: "$credits.value"}, debt: {$sum: "debts.value"}}
     }, {
       $group: {_id: null, credit: {$sum: "$credit"}, debt: {$sum: "$debt"}}
     }, {
       $project: {_id: 0, credit: 1, debt: 1}
-    }, function(error, result) {
+    }], function(error, result) {
       if(error) {
         res.status(500).json({errors: [error]})
       } else {
